9d580ae1763ecf1502f71c4b4ce59badff39fcf7
AnuradhaMishra <anuradhamishra191998@gmail.com>
2021-09-08 12:14:10 +0530

    intial commit

---
+++ src/main/java/revature_training/git_jenkins_docker_demo1/App.java
@@ -0,0 +1,13 @@
+package revature_training.git_jenkins_docker_demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
